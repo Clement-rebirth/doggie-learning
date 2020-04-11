a78cac34e7f7268829032ac91b9078f58317c6be
Clément pat <clem.patigny@gmail.com>
2020-04-11 23:06:48 +0200

    edit 1 article and add 1

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -2,91 +2,109 @@ let articles = [
     {
         title: "Comment approcher un chien",
         summary: "Apprenez à approcher un chien correctement afin d'éviter qu'il se sente aggresser",
-        date: "31/03/2020",
+        date: "11/04/2020",
         image: "dog-on-his-back.jpg",
         imageInfo: "A dog on his back",
         slug: "comment-approcher-un-chien",
-        sources: [],
-        content: `"Contenu de test à mettre en markdown"`
+        sources: [
+            'https://www.chien-visiteur.fr/education-chien-visiteur-comment-saluer-un-chien.php',
+            'https://www.pinterest.fr/pin/559642691176779616/'
+        ],
+        content: `
+            <p>Approcher un chien de la mauvaise manière pourrait l'amener à vous <strong>mordre</strong>. C'est pourquoi il est primordial que vous (ou votre enfant) sachiez comment faire.</p>
+            <h3>Il faut</h3>
+            <ul>
+            <li>demandez au propriétaire si on peut le caresser</li>
+            <li>regarder indirectement</li>
+            <li>se placer à coté</li>
+            <li>s'accroupir</li>
+            <li>caresser le côté ou le dos</li>
+            </ul>
+            <h3 id="ilnefautpas">Il ne faut pas</h3>
+            <ul>
+            <li>le fixer droit dans les yeux</li>
+            <li>crier ou parler trop fort près de lui</li>
+            <li>lui tenir la tête pour l'embrasser</li>
+            <li>l'attraper dans ses bras</li>
+            <li>se pencher sur lui</li>
+            <li>le caresser sur la tête ou le visage</li>
+            <li>courir vers lui</li>
+            <li>faire des mouvements brusques</li>
+            </ul>
+        `
     },
     {
         title: "Les chiens et la chaleur",
         summary: "Bien connaître les effets de la chaleur sur nos chiens est primordiale pour savoir comment agir",
-        date: "31/03/2020",
+        date: "07/04/2020",
         image: "dog-sun.jpg",
         imageInfo: "A dog under the sun",
         slug: "les-chiens-et-la-chaleur",
         sources: [
             'https://www.chien-visiteur.fr/education-chien-visiteur-ne-faites-pas-cuire-votre-chien.php',
             'https://www.comment-economiser.fr/astuce-pour-savoir-s-il-fait-trop-chaud-pour-promener-chien.html',
-            'https://www.chien-visiteur.fr/sante-du-chien.php#mxUg4f9x'
+            'https://www.chien-visiteur.fr/sante-du-chien.php#mxUg4f9x',
+            'https://conseils-veto.com/coup-de-chaleur-chien-coup-de-chaleur-chat'
         ],
         content: `
-        <p><a href="https://www.chien-visiteur.fr/_media/img/xlarge/canicule-chien.jpg"><img src="https://www.chien-visiteur.fr/_media/img/xlarge/canicule-chien.jpg" /></a></p>
-        <p><a href="https://i.pinimg.com/236x/97/eb/57/97eb57b7bc88046ceee3bbdf022d33ba.jpg"><img src="https://i.pinimg.com/236x/97/eb/57/97eb57b7bc88046ceee3bbdf022d33ba.jpg" /></a></p>
-        <h3>Br&ucirc;lures des coussinnets</h3>
-        <hr />
-        <p><a href="https://www.chien-visiteur.fr/_media/img/large/chien-canicule.jpg"><img src="https://www.chien-visiteur.fr/_media/img/large/chien-canicule.jpg" /></a></p>
-        <p>ATTENTION AU SABLE MAIS AUSSI A L'ASPHALTE!! Pour les chiens, les promenades peuvent vite tourner au cauchemar. La temp&eacute;rature du bitume peut en effet atteindre 50&deg;C au sol &raquo;.</p>
-        <p>Sous le soleil d&rsquo;&eacute;t&eacute;, l&rsquo;asphalte des trottoirs et des rues peut atteindre des temp&eacute;ratures qui peuvent bruler les pattes des chiens. Pour les balades pr&eacute;f&eacute;rez donc les moments les plus frais de la journ&eacute;e, c&rsquo;est-&agrave;-dire le matin et le soir apr&egrave;s 20 heures.</p>
-        <p>Si malheureusement vous n&rsquo;avez pas le choix et ne pouvez le promener que dans la journ&eacute;e, des chaussons peuvent prot&eacute;ger ses coussinets..</p>
-        <p>Avant la sortie placez toujours votre main sur l&rsquo;asphalte pendant trente secondes &ndash; si c&rsquo;est trop chaud pour vous, c&rsquo;est &eacute;galement trop chaud pour les pattes de votre chien. Si vous ne supportez pas de laisser la main sur l&rsquo;asphalte pendant 30 secondes, les pattes de votre chien ne supporteront pas non plus une promenade d&rsquo;une demi heure, voire davantage.</p>
-        <p>De mani&egrave;re g&eacute;n&eacute;rale pendant l&rsquo;&eacute;t&eacute;, surveillez bien ses coussinets pour v&eacute;rifier qu&rsquo;ils ne pr&eacute;sentent pas de br&ucirc;lures. Quant &agrave; la plage, vous l&rsquo;&eacute;viterez &eacute;videmment durant les heures les plus chaudes, &agrave; savoir entre 12 h et 16h. Renouvelez r&eacute;guli&egrave;rement l&rsquo;eau de votre animal (qui doit toujours disposer d'eau &agrave; volont&eacute;, toute l'ann&eacute;e).</p>
-        <h3>Chaleur et voiture</h3>
-        <hr />
-        <p>Une vigilance toute particuli&egrave;re doit &ecirc;tre apport&eacute;e lorsqu&rsquo;on est contraint de transporter son chien en voiture saison estivale.</p>
-        <ul>
-        <li>mettre la climatisation ou ventiler</li>
-        <li>faire des haltes &agrave; l&rsquo;ombre</li>
-        <li>proposer r&eacute;guli&egrave;rement de l&rsquo;eau fra&icirc;che</li>
-        </ul>
-        <p>Contrairement &agrave; nous, les chiens ne transpirent pas de tout leur corps. Seules leurs pattes peuvent &eacute;vacuer de la transpiration. Ils doivent donc haleter pour se rafra&icirc;chir, un coup de chaud est in&eacute;vitable et peut &ecirc;tre fatal. Ce ph&eacute;nom&egrave;ne est responsable de nombreux d&eacute;c&egrave;s chez les canid&eacute;s chaque ann&eacute;e.</p>
-        <p>En &eacute;t&eacute; lors de fortes chaleurs, l&rsquo;air peut alors facilement atteindre 40 &agrave; 50&deg; (m&ecirc;me &agrave; l'ombre). Une temp&eacute;rature extr&ecirc;mement difficile &agrave; supporter, en particulier au sein d&rsquo;un espace aussi restreint qu&rsquo;un habitacle de voiture. Laisser une fen&ecirc;tre entrouverte de quelques centim&egrave;tres ou se garer &agrave; l'ombre ne change quasiment rien &agrave; la temp&eacute;rature &agrave; l'int&eacute;rieur de la voiture. Un chien peut mourir d'une insolation en 15 minutes. De plus, dans un d&eacute;lai tr&egrave;s court, une insolation peut entra&icirc;ner de graves l&eacute;sions c&eacute;r&eacute;brales.</p>
-        <p>signes d'insolation :</p>
-        <ul>
-        <li>hal&egrave;te</li>
-        <li>bave de mani&egrave;re excessive</li>
-        <li>vomit</li>
-        <li>somnolent ou l&eacute;thargique</li>
-        </ul>
-        <p><strong>Vous venez d&rsquo;apercevoir un chien enferm&eacute; dans une voiture au soleil ?</strong></p>
-        <ul>
-        <li>
-        <p>V&eacute;rifiez que les ma&icirc;tres ne sont pas aux alentours. Sinon, appelez le commissariat ou la gendarmerie, d&eacute;crivez la sc&egrave;ne et demandez l&rsquo;assistance d&rsquo;un repr&eacute;sentant de l&rsquo;ordre.</p>
-        </li>
-        <li>
-        <p>Imm&eacute;diatement apr&egrave;s, appelez une association locale de protection pour les informer du probl&egrave;me et du fait que vous risquez d&rsquo;&ecirc;tre amen&eacute; &agrave; briser la vitre de la voiture dans laquelle est enferm&eacute; un animal en souffrance.</p>
-        </li>
-        <li>
-        <p>Si les policiers tardent &agrave; arriver et que la situation devient critique, demandez &agrave; deux t&eacute;moins de rester aupr&egrave;s de vous (ils t&eacute;moigneront de votre bonne foi si besoin est, fa&icirc;tes des photos), cassez une vitre et sortez l&rsquo;animal de la voiture surchauff&eacute;e.</p>
-        </li>
-        <li>
-        <p>Lorsque vous avez sorti le chien de la voiture, emmenez-le imm&eacute;diatement dans un endroit ombrag&eacute; ou climatis&eacute;.&nbsp;<strong>N'utilisez pas d'eau glac&eacute;e ou froide</strong>, car le choc des temp&eacute;ratures pourrait causer un malaise. Essayez plut&ocirc;t d'arroser le chien avec de l'eau ti&egrave;de ou posez sur lui des serviettes mouill&eacute;es, surtout au niveau de l'aine, de l'estomac, du poitrail et des pattes. Donnez au chien de petites quantit&eacute;s d'eau fra&icirc;che &agrave; boire. Une fois que la respiration du chien est de nouveau stable.</p>
-        </li>
-        <li>
-        <p>Restez aupr&egrave;s de lui jusqu&rsquo;&agrave; l&rsquo;arriv&eacute;e des forces de l&rsquo;ordre.</p>
-        </li>
-        <li>
-        <p>Expliquez les circonstances et produisez vos t&eacute;moins.</p>
-        </li>
-        <li>
-        <p>Emmenez le chien chez le v&eacute;t&eacute;rinaire le plus proche en faisant savoir qu'il s'agit d'un&nbsp;<strong>cas d'urgence</strong>.</p>
-        <p>Malheureusement cette action engageant votre responsabilit&eacute;, le propri&eacute;taire du v&eacute;hicule pourra se retourner contre vous et porter plainte pour &laquo; effraction &raquo;,&nbsp;<strong>il est donc important de trouver des t&eacute;moins ou prendre des photos. L&rsquo;association contact&eacute;e vous &eacute;paulera si on vous demande des comptes et sachez qu&rsquo;elle est seule habilit&eacute;e &agrave; porter plainte contre le propri&eacute;taire de l&rsquo;animal secouru.</strong></p>
-        </li>
-        </ul>
-        <p>Ne laissez JAMAIS votre chien enferm&eacute; dans votre v&eacute;hicule, m&ecirc;me pour quelques minutes, m&ecirc;me lors de temp&eacute;ratures douces.</p>
+            <ul>
+            <li>Les chiens ne transpirent pas, il se rafraîchissent par les poumons en haletant</li>
+            <li>Votre chien ne doit <strong>pas être soumis à un effort physique important</strong> lorsque la température extérieure dépasse les 22°C</li>
+            <li><strong>il ne faut pas tondre votre animal</strong>, sa fourure le garde au chaud en hiver et <strong>au frais en été</strong> ! Elle protège également des rayons nocifs du soleils</li>
+            </ul>
+            <h3>Coup de chaleur</h3>
+            <hr>
+            <p><strong>Un coup de chaleur peut survenir en moins de 5 minutes</strong> ! C’est pourquoi on ne laisse <strong>jamais un chien dans une voiture</strong> lorsqu’il fait chaud, même pas quelques minutes ! Si vous voyez un animal enfermé dans un véhicule, essayez de trouver le propriété au plus vite ou prévenez un agent de police.</p>
+            <p>Symptômes (liste non exhaustive) :</p>
+            <ul>
+            <li>respiration rapide</li>
+            <li>halètement important</li>
+            <li>faiblesse</li>
+            <li>langue ou gencives rouge foncé ou bleues</li>
+            <li>vomissements</li>
+            <li>diarhées</li>
+            <li>manque d’équilibre</li>
+            <li>convulsions</li>
+            <li>la mort</li>
+            </ul>
+            <p>Que faire si votre chien souffre d’un coup de chaleur ?</p>
+            <ul>
+            <li>le sortir de l’environnement ayant causé ce problème, puis le <strong>mettre au frais</strong></li>
+            <li><strong>contacter un vétérinaire</strong></li>
+            <li>faire baisser sa température <strong>progressivement</strong>, en le mouillant avec de l’eau tiède/fraîche mais pas glacée !</li>
+            <li>appliquer des serviettes humides</li>
+            <li>s’il peut boire, donnez-lui de l’eau sans le forcer</li>
+            </ul>
+            <h3>Les coussinnets</h3>
+            <hr>
+            <p><strong>Attention au sable mais aussi à l’asphalte</strong> !</p>
+            <ul>
+            <li>l’asphalte des trottoirs peut atteindre des températures pouvant <strong>brûler</strong> les pâtes des chiens !</li>
+            <li>baladez le aux moments les plus frais, le matin et le soir</li>
+            <li>si malheureusement vous n’avez pas le choix et ne pouvez le promener que dans la journée, des chaussons peuvent protéger ses coussinets</li>
+            <li>avant la sortie placez toujours votre main sur l’asphalte pendant 30 secondes; si c’est trop chaud pour vous, c’est également trop chaud pour les pattes de votre chien</li>
+            </ul>
+            <p>De manière générale pendant l’été, surveillez régulièrement l’état de ses coussinets.</p>
+            <h3>Chaleur et voiture</h3>
+            <hr>
+            <p>Une très grande vigilance toute doit être apportée lorsqu’on est obligé de transporter son chien en voiture lorsqu’il fait chaud.</p>
+            <ul>
+            <li>mettre la climatisation/ventiler</li>
+            <li>faire des pauses à l’ombre</li>
+            <li>proposer régulièrement de l’eau fraîche</li>
+            </ul>   
         `
     },
-    {
-        title: "Êtes-vous sûr de vraiment vouloir un chien ?",
-        summary: "Nombreux sont les chiens abandonnés chaque année, savez-vous vraiment ce qu'avoir un chien engage ?",
-        date: "31/03/2020",
-        image: "dog-with-woman.jpg",
-        imageInfo: "A woman who's hugging her Husky",
-        slug: "voulez-vous-vraiment-un-chien",
-        sources: [],
-        content: `Contenu de test à mettre en markdown`
-    }
+    // {
+    //     title: "Êtes-vous sûr de vraiment vouloir un chien ?",
+    //     summary: "Nombreux sont les chiens abandonnés chaque année, savez-vous vraiment ce qu'avoir un chien engage ?",
+    //     date: "31/03/2020",
+    //     image: "dog-with-woman.jpg",
+    //     imageInfo: "A woman who's hugging her Husky",
+    //     slug: "voulez-vous-vraiment-un-chien",
+    //     sources: [],
+    //     content: `Contenu de test à mettre en markdown`
+    // }
 ];
 
 export default articles;
